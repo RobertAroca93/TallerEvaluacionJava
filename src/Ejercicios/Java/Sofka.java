905b5a39bdc31479cf10916d34b88154b20a4d5b
Robert <rjaroca@corhuila.edu.co>
2022-02-12 17:20:51 -0500

    Pimer ejercicio, declarar variable e indicar cual es mayor o igual.

---
+++ src/Ejercicios/Java/Sofka.java
@@ -0,0 +1,32 @@
+package Ejercicios.Java;
+
+public class Sofka {
+
+    public static void main(String[] args) {
+        //Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos. Si son iguales indicarlo también. Ve cambiando los valores para comprobar que funciona
+
+        int v1= 9;
+        int v2= 20;
+
+        if(v1>= v2){
+            if(v1==v2){
+                System.out.println("la variable "+ v1 +"y" + v2 + "son iguales");
+            }else{
+                System.out.println("la variable "+ v1 + "es mayor que "+ v2);
+            }
+        }else {
+            System.out.println(" la variable " + v2 + " es mayor que "+v1);
+        }
+
+        //Agregar entrada por consola de dos valores e indicar si son mayores, menores o iguales.
+
+        
+
+
+
+
+
+
+
+    }
+}
