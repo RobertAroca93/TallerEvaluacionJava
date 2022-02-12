983ef6b4e6fba7df42f0962a06568a2b9b2c6bc4
Robert <rjaroca@corhuila.edu.co>
2022-02-12 18:03:54 -0500

    se agrega al primer ejercicio la funcio de pedir datos por consola.

--- src/Ejercicios/Java/Sofka.java
+++ src/Ejercicios/Java/Sofka.java
@@ -1,26 +1,35 @@
 package Ejercicios.Java;
+import java.util.Scanner;
 
 public class Sofka {
 
     public static void main(String[] args) {
-        //Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos. Si son iguales indicarlo también. Ve cambiando los valores para comprobar que funciona
+        //Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos. Si son iguales indicarlo también. Ve cambiando los valores para comprobar que funciona.
+        //Agregar entrada por consola de dos valores e indicar si son mayores, menores o iguales.
 
-        int v1= 9;
-        int v2= 20;
+        Scanner teclado = new Scanner(System.in);
+
+        int v1= 2;
+        int v2= 9;
+
+        System.out.println("Ingrese el primer valor");
+        v1=teclado.nextInt();
+        System.out.println("Ingrese el segundo valor");
+        v2=teclado.nextInt();
+
 
         if(v1>= v2){
             if(v1==v2){
-                System.out.println("la variable "+ v1 +"y" + v2 + "son iguales");
+                System.out.println("la variable " + v1 +" y " + v2 + " son iguales");
             }else{
-                System.out.println("la variable "+ v1 + "es mayor que "+ v2);
+                System.out.println("la variable "+ v1 + " es mayor que "+ v2);
             }
         }else {
-            System.out.println(" la variable " + v2 + " es mayor que "+v1);
+            System.out.println(" la variable " + v1 + " es menor que "+v2);
         }
 
         //Agregar entrada por consola de dos valores e indicar si son mayores, menores o iguales.
 
-        
 
 
 
