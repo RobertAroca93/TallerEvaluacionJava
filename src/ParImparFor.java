2eb4d622e652e78bec8b6d8190d60b1b5f9b904b
Robert <rjaroca@corhuila.edu.co>
2022-02-12 21:03:05 -0500

    se agrega ejercicio de numeros pares e impares del 1 al 100 usando ciclo for.

---
+++ src/ParImparFor.java
@@ -0,0 +1,16 @@
+public class ParImparFor {
+
+    public static void main(String[] args) {
+
+
+
+        for(int num=1;num<=100;num++){
+            if(num%2==0){
+                System.out.println(num  + " es un numero par");
+            }else {
+                System.out.println(num  + " es un numero impar");
+            }
+        }
+
+    }
+}
