ba5db34de24056a37c9bdd2467e76bd90b417e1e
Robert <rjaroca@corhuila.edu.co>
2022-02-13 11:50:13 -0500

    Se agrega funcion que cambia las letras de un texto.

---
+++ src/Ejercicios/Java/AppCambiaLetras.java
@@ -0,0 +1,14 @@
+//Del texto, “La sonrisa sera la mejor arma contra la tristeza” Reemplaza todas las a del String anterior por una e, adicionalmente concatenar a esta frase una adicional que ustedes quieran incluir por consola y las muestre luego unidas.
+
+package Ejercicios.Java;
+
+public class AppCambiaLetras {
+    public static void main(String[] args) {
+
+        String cadena = "La sonrisa sera la mejor arma contra la tristeza";
+        System.out.println(cadena.replace("a","e"));
+
+        String cadena2 = " y la maldad";
+        System.out.println(cadena + cadena2);
+    }
+}
