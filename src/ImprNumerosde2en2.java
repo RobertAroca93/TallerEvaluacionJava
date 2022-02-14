5ac297c7c6ca02786d734b47323ca8535a7ff80c
Robert <rjaroca@corhuila.edu.co>
2022-02-13 21:47:27 -0500

    se agrega funcion que toma numero por consola y lo suma de 2 en 2 hasta 1000.

---
+++ src/ImprNumerosde2en2.java
@@ -0,0 +1,15 @@
+//Crear un programa que pida un numero por teclado y que imprima por pantalla los números desde el numero introducido hasta 1000 con saldos de 2 en 2.
+import java.util.Scanner;
+
+public class ImprNumerosde2en2 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese un valor entre 0 y 999");
+        int valor = teclado.nextInt();
+
+
+        }
+    }
+
+
